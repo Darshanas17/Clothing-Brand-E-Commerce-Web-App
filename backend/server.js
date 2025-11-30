ccdd738a919c5bcee8f738ec808d8b0c5685562f
Darshanas17 <darshanas1234@gmail.com>
2025-11-30 15:54:12 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
@@ -14,12 +13,6 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
 
 app.get("/", (req, res) => {
   res.json({
